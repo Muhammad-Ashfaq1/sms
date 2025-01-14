5f9d772fc48d8974681c87a1fff64f456056c7b8
Muhammad-Ashfaq1 <mitf19e032@gmail.com>
2025-01-14 18:58:22 +0500

    send-test-email

---
+++ resources/views/emails/test.blade.php
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h1>Muhammad Ashfaq</h1>
+<p>If you're seeing this, the email system is working fine!</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,16 @@ use App\Http\Controllers\Admin\OrganizationController;
 use App\Http\Controllers\Admin\SchoolController;
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/send-test-email', function () {
+    $to = 'recipient@example.com'; // Change this to the recipient's email
+
+    \Illuminate\Support\Facades\Mail::to($to)->send(new \App\Mail\TestEmail());
+
+    return "Test email sent to {$to}";
+});
+
+
 // Central domain routes
 foreach (config('tenancy.central_domains') as $domain) {
     Route::domain($domain)->group(function () {
@@ -14,6 +24,7 @@ foreach (config('tenancy.central_domains') as $domain) {
 
         /*routes for super admin*/
         require base_path('routes/super-admin.php');
+
 
         // Central home route
         Route::get('/', function () {
